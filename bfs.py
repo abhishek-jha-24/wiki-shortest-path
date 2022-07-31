dd2334ac40be5e6671f1fd220b55b4f0fa64af85
abhishek-jha-24 <65348523+abhishek-jha-24@users.noreply.github.com>
2022-07-31 11:32:17 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,88 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
+
+
+
+
+def get_main_link(url):
+	ans = ""
+	for i in range(0, len(url)):
+		if url[i] == "#":
+			break
+		ans += url[i]
+	return ans
+
+
+
+def get_next_nodes(url):
+
+	req = Request(url)
+	links = []
+	try:
+	
+		html_page = urlopen(req)
+
+		soup = BeautifulSoup(html_page, "lxml")
+		for link in soup.findAll('a'):
+			temp = link.get('href')
+			
+			try:
+				if ".org" not in temp:
+					temp = url[:24]+temp
+				elif "https:" not in temp:
+					temp = "https:" + temp
+				else:
+					pass
+				if temp[-1] == "/":
+					temp = temp[:-1]
+				temp = get_main_link(temp)
+				
+				links.append(temp)
+			except:
+				pass
+	except:
+		pass
+	
+	return links
+
+
+
+
+def find_path(source, destination):
+	queue = []
+	queue.append(source)
+	k = 0
+	while (len(queue) > 0):
+		temp = len(queue)
+		for i in range(0, temp):
+
+			curr = queue.pop(0)
+			if curr == destination:
+				return k
+			next_nodes = get_next_nodes(curr)
+
+			for j in next_nodes:
+				try:
+					
+					if j[0] != 'h':
+						j = "https:"+j
+					
+					queue.append(j)
+				except:
+					pass
+		k+=1
+	return k
+
+
+source = "https://en.wikipedia.org/wiki/Tic-tac-toe"
+destination = "https://en.wikipedia.org/wiki/Game_complexity"
+
+if destination[-1] == "/":
+	destination = destination[:-1]
+l = get_next_nodes(source)
+if destination in l:
+	print("yes")
+
+print("shortest path: ", find_path(source, destination))
+
